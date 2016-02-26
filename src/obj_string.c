32ccc9a85b09da354859c3f22b7b02bb56249ecf
Erik <erik.svedang@gmail.com>
2016-02-26 17:34:08 +0100

    automatic printing of pointers to Array:s (the one defined in "shared.h")

--- src/obj_string.c
+++ src/obj_string.c
@@ -1,6 +1,7 @@
 #include "obj_string.h"
 #include "env.h"
 #include "eval.h"
+#include "gc.h"
 
 bool setting_print_lambda_body = true;
 
@@ -143,6 +144,7 @@ void obj_to_string_internal(Obj *total, const Obj *o, bool prn, int indent) {
     if(o->meta && (type_lookup = env_lookup(o->meta, obj_new_keyword("type")))) {
       if(type_lookup->tag == 'C' && type_lookup->cdr->car && obj_eq(type_lookup->car, obj_new_keyword("Array"))) {
 
+	shadow_stack_push(total);
 	shadow_stack_push(type_lookup);
 	
 	Obj *reffed_arg_type = obj_list(obj_new_keyword("ref"), type_lookup); // HACK: ref needed when sending arrays into str
@@ -150,34 +152,40 @@ void obj_to_string_internal(Obj *total, const Obj *o, bool prn, int indent) {
 	Obj *signature = obj_list(obj_new_keyword("fn"), args_type, type_string);
 	Obj *quoted_sig = obj_list(lisp_quote, signature);
 
-	printf("quoted_sig: %s\n", obj_to_string(quoted_sig)->s);
+	//printf("quoted_sig: %s\n", obj_to_string(quoted_sig)->s);
 	
 	Obj *call_to_generic_name = obj_list(obj_new_symbol("generic-name"), obj_new_string("str"), quoted_sig);
 
 	shadow_stack_push(call_to_generic_name);
 	Obj *generic_name_result = eval(global_env, call_to_generic_name);
 	shadow_stack_push(generic_name_result);
+
 	if(eval_error) {
 	  printf("Error when calling generic-name:\n");
 	  printf("%s\n", obj_to_string(eval_error)->s);
 	  return;
 	}
-	char *generic_name = obj_to_string_not_prn(generic_name_result)->s;
-	printf("Generic name: %s\n", generic_name);
+	else {
+	  //printf("Generic name: %s\n", obj_to_string_not_prn(generic_name_result)->s);
+	}
 
 	// Also make sure this particular version of the str primop has been baked:
 	Obj *call_to_bake_generic_primop_auto = obj_list(obj_new_symbol("bake-generic-primop-auto"), obj_new_string("str"), quoted_sig);
 	shadow_stack_push(call_to_bake_generic_primop_auto);
 	eval(global_env, call_to_bake_generic_primop_auto);
+
 	if(eval_error) {
 	  printf("Error when calling bake-generic-primop-auto:\n");
 	  printf("%s\n", obj_to_string(eval_error)->s);
 	  return;
 	}
 	else {
-	  printf("%s should now exists\n", generic_name);
+	  //printf("%s should now exists\n", obj_to_string_not_prn(generic_name_result)->s);
 	}
-	
+
+	char *generic_name = obj_to_string_not_prn(generic_name_result)->s;
+	//printf("generic_name 1: %s\n", generic_name);
+
 	Obj *call_to_str = obj_list(obj_new_symbol(generic_name), (struct Obj*)o);
 
 	//   OBS!!!
@@ -186,7 +194,7 @@ void obj_to_string_internal(Obj *total, const Obj *o, bool prn, int indent) {
 	//   printf("Call to str: %s\n", obj_to_string(call_to_str)->s);
 	//
 	//   DON'T DO IT!!!
-	
+
 	shadow_stack_push(call_to_str);
 	Obj *array_to_string_result = eval(global_env, call_to_str);
 	shadow_stack_push(array_to_string_result);
@@ -197,7 +205,7 @@ void obj_to_string_internal(Obj *total, const Obj *o, bool prn, int indent) {
 	  obj_string_mut_append(total, "FAIL");
 	  return;
 	}
-	obj_string_mut_append(total, obj_to_string(array_to_string_result)->s);
+	obj_string_mut_append(total, obj_to_string_not_prn(array_to_string_result)->s);
 
 	shadow_stack_pop();
 	shadow_stack_pop();
@@ -205,11 +213,8 @@ void obj_to_string_internal(Obj *total, const Obj *o, bool prn, int indent) {
 	shadow_stack_pop();
 	shadow_stack_pop();
 	shadow_stack_pop();
+	shadow_stack_pop();
 
-	/* obj_string_mut_append(total, "<ptr to Array of "); */
-	/* obj_string_mut_append(total, obj_to_string(type_lookup->cdr->car)->s); */
-	/* obj_string_mut_append(total, ">"); */
-	
 	return;
       }
       else {
