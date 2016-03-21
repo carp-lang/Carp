66acbedf416b671ef786c039f87ad5a3cd8ce9ed
Erik <erik.svedang@gmail.com>
2016-03-21 07:12:22 +0100

    inf-carp-mode

--- emacs/carp-mode.el
+++ emacs/carp-mode.el
@@ -1,36 +1,6 @@
-(defvar carp-cli-file-path "carp-inferior")
-(defvar carp-cli-arguments '())
 
-(defun run-carp ()
-  "Run an inferior instance of `carp-inferior' inside Emacs."
-  (interactive)
-  (let* ((carp-program carp-cli-file-path)
-         (buffer (comint-check-proc "Carp")))
-    ;; Pop to the "*Carp*" buffer if the process is dead, the buffer is missing or it's got the wrong mode.
-    (pop-to-buffer-same-window
-     (if (or buffer (not (derived-mode-p 'carp-mode))
-             (comint-check-proc (current-buffer)))
-         (get-buffer-create (or buffer "*Carp*"))
-       (current-buffer)))
-    ;; Create the comint process if there is no buffer.
-    (unless buffer
-      (apply 'make-comint-in-buffer "Carp" buffer
-             carp-program carp-cli-arguments)
-      (carp-mode))))
-
-(defun carp-test-1 ()
-  (interactive)
-  (comint-send-string "*Carp*" "(println (str 12345))\n"))
-
-(defun carp-eval-buffer ()
-  (interactive)
-  (comint-send-string "*Carp*" (buffer-string)))
-
-(defvar carp-mode-map
-  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
-    (define-key map (kbd "C-c C-l") 'carp-eval-buffer)
-    map)
-  "Basic mode map for `carp-mode'")
-
-(define-derived-mode carp-mode comint-mode "Carp"
+(define-derived-mode carp-mode clojure-mode "Carp"
   "Major mode for the Carp programming language.")
+
+(add-to-list 'auto-mode-alist '("\\.carp\\'" . carp-mode))
+
