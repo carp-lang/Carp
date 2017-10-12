3b0ff523eb5e69da758a56ecc008910b6b7bbdd7
Erik Svedäng <erik.svedang@gmail.com>
2017-10-12 17:02:00 +0200

    Array tests work again.

--- run_carp_tests.sh
+++ run_carp_tests.sh
@@ -9,7 +9,7 @@ stack exec Carp ./examples/polymorphic.carp;
 stack exec Carp ./examples/selection.carp;
 stack exec Carp ./examples/functor.carp;
 stack exec Carp ./examples/vec2.carp;
-#stack exec Carp ./examples/array.carp;
+stack exec Carp ./examples/array.carp;
 
 # Game will run until closed:
 stack exec Carp ./examples/game.carp;
