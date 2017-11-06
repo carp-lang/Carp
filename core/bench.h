f794d7685fe3004a49a1879e25912a8fb9f65fbc
hellerve <veit@veitheller.de>
2017-11-06 20:23:33 +0100

    bench: added nanosecond accuracy

--- core/bench.h
+++ core/bench.h
@@ -1,8 +1,7 @@
 #include <sys/time.h>
 
-// TODO: use timespecs for better accuracy!
 double get_MINUS_time_MINUS_elapsed() {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return 1000000 * tv.tv_sec + tv.tv_usec;
+  struct timespec tv;
+  clock_gettime(CLOCK_REALTIME, &tv);
+  return 1000000000 * tv.tv_sec + tv.tv_nsec;
 }
