046956d711e68ffa6703493aae77eb7f6c907be2
hellerve <veit@veitheller.de>
2017-11-06 11:37:24 +0100

    bench: renamed bench macro to bench-n and started working on real bench fn

--- core/bench.h
+++ core/bench.h
@@ -1,7 +1,8 @@
 #include <sys/time.h>
 
+// TODO: use timespecs for better accuracy!
 double get_MINUS_time_MINUS_elapsed() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  return 1000000 * tv.tv_sec + tv.tv_usec;;
+  return 1000000 * tv.tv_sec + tv.tv_usec;
 }
