2f4505bf73a92f9c4b9a26eeeb3477bde274face
hellerve <veit@veitheller.de>
2017-12-18 12:30:14 +0100

    core: added char-at to String

--- core/core.h
+++ core/core.h
@@ -248,6 +248,10 @@ string String_str(string *s) {
     return buffer;
 }
 
+char String_char_MINUS_at(string s, int i) {
+  return s[i];
+}
+
 Array String_chars(string *s) {
     Array chars;
     chars.len = strlen(*s);
