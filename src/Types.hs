badcb7e43fa75095f2d1883d17f7ce1874365a1b
Erik Svedäng <erik.svedang@gmail.com>
2018-01-25 23:00:26 +0100

    Bring back assertion.

--- src/Types.hs
+++ src/Types.hs
@@ -134,7 +134,7 @@ mangle = replaceChars (Map.fromList [('+', "_PLUS_")
 unifySignatures :: Ty -> Ty -> TypeMappings
 unifySignatures v t = Map.fromList (unify v t)
   where unify :: Ty -> Ty -> [(String, Ty)]
-        --unify a@(VarTy _) b@(VarTy _) = error ("Can't unify " ++ show a ++ " with " ++ show b)
+        unify a@(VarTy _) b@(VarTy _) = error ("Can't unify " ++ show a ++ " with " ++ show b)
 
         unify (VarTy a) value = [(a, value)]
 
