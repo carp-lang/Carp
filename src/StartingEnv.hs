f140c3534914c3029e0f041e5f05117e550f0d33
Erik Svedäng <erik.svedang@gmail.com>
2018-02-06 12:58:16 +0100

    Comments in the StartingEnv module.

--- src/StartingEnv.hs
+++ src/StartingEnv.hs
@@ -12,6 +12,7 @@ import Commands
 import Parsing
 import Eval
 
+-- | These modules will be loaded in order before any other code is evaluated.
 coreModules :: String -> [String]
 coreModules carpDir = map (\s -> carpDir ++ "/core/" ++ s ++ ".carp") [ "Interfaces"
                                                                       , "Macros"
@@ -29,6 +30,7 @@ coreModules carpDir = map (\s -> carpDir ++ "/core/" ++ s ++ ".carp") [ "Interfa
                                                                       , "System"
                                                                       ]
 
+-- | The array module contains functions for working with the Array type.
 arrayModule :: Env
 arrayModule = Env { envBindings = bindings, envParent = Nothing, envModuleName = Just "Array", envUseModules = [], envMode = ExternalEnv }
   where bindings = Map.fromList [ templateNth
@@ -47,10 +49,12 @@ arrayModule = Env { envBindings = bindings, envParent = Nothing, envModuleName =
                                 , templateSort
                                 ]
 
+-- | The pointer module contains functions for dealing with pointers.
 pointerModule :: Env
 pointerModule = Env { envBindings = bindings, envParent = Nothing, envModuleName = Just "Pointer", envUseModules = [], envMode = ExternalEnv }
   where bindings = Map.fromList [ templatePointerCopy ]
 
+-- | A template function for copying (= deref:ing) any pointer.
 templatePointerCopy :: (String, Binder)
 templatePointerCopy = defineTemplate
   (SymPath ["Pointer"] "copy")
@@ -61,14 +65,7 @@ templatePointerCopy = defineTemplate
                         ,"}"])
   (const [])
 
-dynamicStringModule :: Env
-dynamicStringModule = Env { envBindings = bindings, envParent = Nothing, envModuleName = Just "String", envUseModules = [], envMode = ExternalEnv }
-  where bindings = Map.fromList [ addCommand "char-at" 2 commandCharAt
-                                , addCommand "index-of" 2 commandIndexOf
-                                , addCommand "substring" 3 commandSubstring
-                                , addCommand "count" 1 commandStringCount
-                                ]
-
+-- | The dynamic module contains dynamic functions only available in the repl and during compilation.
 dynamicModule :: Env
 dynamicModule = Env { envBindings = bindings, envParent = Nothing, envModuleName = Just "Dynamic", envUseModules = [], envMode = ExternalEnv }
   where bindings = Map.fromList $
@@ -109,6 +106,16 @@ dynamicModule = Env { envBindings = bindings, envParent = Nothing, envModuleName
                     ]
                     ++ [("String", Binder (XObj (Mod dynamicStringModule) Nothing Nothing))]
 
+-- | A submodule of the Dynamic module. Contains functions for working with strings in the repl or during compilation.
+dynamicStringModule :: Env
+dynamicStringModule = Env { envBindings = bindings, envParent = Nothing, envModuleName = Just "String", envUseModules = [], envMode = ExternalEnv }
+  where bindings = Map.fromList [ addCommand "char-at" 2 commandCharAt
+                                , addCommand "index-of" 2 commandIndexOf
+                                , addCommand "substring" 3 commandSubstring
+                                , addCommand "count" 1 commandStringCount
+                                ]
+
+-- | The global environment before any code is run.
 startingGlobalEnv :: Bool -> Env
 startingGlobalEnv noArray =
   Env { envBindings = bindings,
@@ -126,7 +133,7 @@ startingGlobalEnv noArray =
                    ++ [("Pointer", Binder (XObj (Mod pointerModule) Nothing Nothing))]
                    ++ [("Dynamic", Binder (XObj (Mod dynamicModule) Nothing Nothing))]
 
-
+-- | The type environment (containing deftypes and interfaces) before any code is run.
 startingTypeEnv :: Env
 startingTypeEnv = Env { envBindings = bindings
                       , envParent = Nothing
@@ -137,7 +144,7 @@ startingTypeEnv = Env { envBindings = bindings
   where bindings = Map.fromList
           $ [ interfaceBinder "copy" (FuncTy [(RefTy (VarTy "a"))] (VarTy "a")) [SymPath ["Array"] "copy", SymPath ["Pointer"] "copy"] builtInSymbolInfo
             , interfaceBinder "str" (FuncTy [(VarTy "a")] StringTy) [SymPath ["Array"] "str"] builtInSymbolInfo
-              -- TODO: Implement! ("=", Binder (defineInterface "=" (FuncTy [(VarTy "a"), (VarTy "a")] BoolTy)))
+            , interfaceBinder "prn" (FuncTy [(VarTy "a")] StringTy) [] builtInSymbolInfo
             ]
         builtInSymbolInfo = Info (-1) (-1) "Built-in." Set.empty (-1)
 
