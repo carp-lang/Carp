ef334048b2ccecedf337435bca2094c561e08113
Erik Svedäng <erik.svedang@gmail.com>
2018-02-27 10:32:25 +0100

    The tool "carp-header-parse" works, at least for simple inputs.

--- examples/parse_me.h
+++ examples/parse_me.h
@@ -1,4 +1,6 @@
+// This is a comment
 void boo();
 char hoo();
+// This is also a comment
 int foo(int x);
 float goo(double x, double y);
--- headerparse/Main.hs
+++ headerparse/Main.hs
@@ -26,8 +26,58 @@ parseHeaderFile :: FilePath -> String -> [XObj]
 parseHeaderFile path src =
   case Parsec.runParser cSyntax () path src of
     Left err -> error (show err)
-    Right ok -> ok
+    Right ok -> concat ok
   where
-        cSyntax :: Parsec.Parsec String () [XObj]
-        cSyntax = do _ <- Parsec.many Parsec.anyChar
-                     return [(XObj (Sym (SymPath [] "foobar") Symbol) Nothing Nothing)]
+        cSyntax :: Parsec.Parsec String () [[XObj]]
+        cSyntax = Parsec.sepBy line (Parsec.char '\n')
+
+          -- do _ <- Parsec.many Parsec.anyChar
+          --            return [(XObj (Sym (SymPath [] "foobar") Symbol) Nothing Nothing)]
+
+        line :: Parsec.Parsec String () [XObj]
+        line = Parsec.try functionPrototype <|> discarded
+
+          --Parsec.choice [functionPrototype, discarded]
+
+        functionPrototype :: Parsec.Parsec String () [XObj]
+        functionPrototype = do returnTypeString <- Parsec.many1 Parsec.letter
+                               Parsec.char ' '
+                               name <- Parsec.many1 Parsec.letter
+                               Parsec.char '('
+                               argTypeStrings <- Parsec.sepBy arg (Parsec.char ',')
+                               Parsec.char ')'
+                               Parsec.char ';'
+                               return [XObj (Lst [ (XObj (Sym (SymPath [] "register") Symbol) Nothing Nothing)
+                                                 , (XObj (Sym (SymPath [] name) Symbol) Nothing Nothing)
+                                                 , toTypeXObj argTypeStrings returnTypeString
+                                                 ]) Nothing Nothing]
+
+        arg :: Parsec.Parsec String () String
+        arg = do Parsec.many (Parsec.char ' ')
+                 argTypeAsString <- Parsec.many1 Parsec.letter
+                 Parsec.many1 (Parsec.char ' ')
+                 _ <- Parsec.many1 Parsec.letter
+                 Parsec.many (Parsec.char ' ')
+                 return argTypeAsString
+
+        discarded :: Parsec.Parsec String () [XObj]
+        discarded = do discardedLine <- Parsec.many (Parsec.noneOf "\n")
+                       return []
+                       --return [(XObj (Str ("DISCARDED: " ++ discardedLine)) Nothing Nothing)]
+
+toTypeXObj :: [String] -> String -> XObj
+toTypeXObj argTypeStrings returnTypeString =
+  (XObj (Lst [ (XObj (Sym (SymPath [] "λ") Symbol) Nothing Nothing)
+             , (XObj (Arr (map (tyToXObj . cTypeToCarpType) argTypeStrings)) Nothing Nothing)
+             , (XObj (Sym (SymPath [] (show (cTypeToCarpType returnTypeString))) Symbol) Nothing Nothing)
+             ]) Nothing Nothing)
+
+cTypeToCarpType :: String -> Ty
+cTypeToCarpType "char" = CharTy
+cTypeToCarpType "int" = IntTy
+cTypeToCarpType "bool" = BoolTy
+cTypeToCarpType "long" = LongTy
+cTypeToCarpType "double" = DoubleTy
+cTypeToCarpType "float" = FloatTy
+cTypeToCarpType "void" = UnitTy
+cTypeToCarpType s = (StructTy s [])
