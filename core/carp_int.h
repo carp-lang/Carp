4c8b661bdc89f0b3b432f8c14fe13bc35459cf53
Erik Svedäng <erik.svedang@gmail.com>
2018-03-06 16:44:07 +0100

    Moved safe int functions into their own file that's not automatically loaded.

--- core/carp_int.h
+++ core/carp_int.h
@@ -5,11 +5,6 @@ int Int__PLUS_(int x, int y)   { return x + y; }
 int Int__MINUS_(int x, int y)  { return x - y; }
 int Int__MUL_(int x, int y)    { return x * y; }
 int Int__DIV_(int x, int y)    { return x / y; }
-#ifndef _WIN32
-bool Int_safe_MINUS_add(int x, int y, int* res) { return __builtin_sadd_overflow(x, y, res); }
-bool Int_safe_MINUS_sub(int x, int y, int* res) { return __builtin_ssub_overflow(x, y, res); }
-bool Int_safe_MINUS_mul(int x, int y, int* res) { return __builtin_smul_overflow(x, y, res); }
-#endif
 bool Int__EQ_(int x, int y)     { return x == y; }
 bool Int__DIV__EQ_(int x, int y) { return x != y; }
 bool Int__LT_(int x, int y)    { return x < y; }
@@ -34,4 +29,3 @@ int Int_mod(int x, int divider) {
 bool Int_mask(int a, int b) {
     return a & b;
 }
-
