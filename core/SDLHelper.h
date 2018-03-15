0f6574193f8a94b41cf2c10002adeb8a663944a4
Erik Svedäng <erik.svedang@gmail.com>
2018-03-15 21:14:18 +0100

    Proper SDL module added, examples updated to work with it.

--- core/SDLHelper.h
+++ core/SDLHelper.h
@@ -33,34 +33,34 @@ void app_MINUS_stop(App *app) {
     SDL_Quit();
 }
 
-SDL_Event SDL_Event_init() {
+SDL_Event SDL_event_MINUS_init() {
     SDL_Event e;
     return e;
 }
 
-int event_MINUS_type(SDL_Event *e) {
+int SDL_event_MINUS_type(SDL_Event *e) {
     return e->type;
 }
 
-SDL_Keycode event_MINUS_keycode(SDL_Event *e) {
+SDL_Keycode SDL_event_MINUS_keycode(SDL_Event *e) {
     return e->key.keysym.sym;
 }
 
-int Keycode__EQ_(SDL_Keycode a, SDL_Keycode b) {
+int SDL_Keycode__EQ_(SDL_Keycode a, SDL_Keycode b) {
     return a == b;
 }
 
-SDL_Keycode Keycode_copy(SDL_Keycode* a) {
+SDL_Keycode SDL_Keycode_copy(SDL_Keycode* a) {
     return *a;
 }
 
-string Keycode_str(SDL_Keycode a) {
+string SDL_Keycode_str(SDL_Keycode a) {
     char *buffer = CARP_MALLOC(32);
     snprintf(buffer, 32, "%d", a);
     return buffer;
 }
 
-SDL_Rect make_MINUS_rect(int x, int y, int w, int h) {
+SDL_Rect SDL_make_MINUS_rect(int x, int y, int w, int h) {
     SDL_Rect r;
     r.x = x;
     r.y = y;
@@ -69,9 +69,13 @@ SDL_Rect make_MINUS_rect(int x, int y, int w, int h) {
     return r;
 }
 
-SDL_Point make_MINUS_point(int x, int y) {
+SDL_Point SDL_make_MINUS_point(int x, int y) {
     SDL_Point p;
     p.x = x;
     p.y = y;
     return p;
+}
+
+bool SDL_EventType__EQ_(SDL_EventType a, SDL_EventType b) {
+    return a == b;
 }
