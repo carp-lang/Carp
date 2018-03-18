21c4d2d2fbda6960560de0b6ee48111a793c8555
Erik Svedäng <erik.svedang@gmail.com>
2018-03-18 15:23:34 +0100

    Fix new function in carp_char.h

--- core/carp_char.h
+++ core/carp_char.h
@@ -16,13 +16,13 @@ char Char_copy(char *c) {
   return *c;
 }
 
-string PtrChar_str(char *c) {
+String PtrChar_str(char *c) {
     size_t len = strlen(c) + 1;
-    string ptr = CARP_MALLOC(len);
+    String ptr = CARP_MALLOC(len);
 
     if (ptr == NULL) {
       return NULL;
     }
 
-    return (string) memcpy(ptr, c, len);
+    return (String) memcpy(ptr, c, len);
 }
