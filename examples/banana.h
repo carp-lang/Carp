eda06f3bc1d3a1e050583b18e882271e27a1a544
Erik Svedäng <erik.svedang@gmail.com>
2018-03-26 08:47:46 +0200

    Reproduced error in bugs.carp

--- examples/banana.h
+++ examples/banana.h
@@ -2,10 +2,14 @@
 #define BANANA_H
 
 // This is an example external struct
-
 typedef struct {
     double price;
     int size;
 } Banana;
+
+// This is an external function that can be registered
+int magic() {
+    return 123456789;
+}
 
 #endif
