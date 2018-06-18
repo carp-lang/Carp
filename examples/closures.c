9002b7b8066cece0e34f14ca16a6fd1b9bf10204
Erik Svedäng <erik.svedang@gmail.com>
2018-06-18 12:20:20 +0200

    New version.

--- examples/closures.c
+++ examples/closures.c
@@ -7,24 +7,24 @@
 //      (f)))
 
 // Environment for this particular lambda
-typedef struct {
+typedef struct Env_main_0 {
     int x;
 } Env_main_0;
 
-// The one struct needed for any lambda with signature (Fn [] Int)
-typedef struct Closure__Unit_Int {
+// Deleter
+void delete_Env_main_0(Env_main_0 env) {
+    // nothing to free in this case
+}
+
+typedef struct Closure {
     int (*callback)(void *closure);
     void *env;
-} Closure__Unit_Int;
+    void (*delete)(void*);
+} Closure;
 
 // The body of the lambda, but lifted to its own function. Takes the environment as a first argument.
 int lifted_lambda_main_0(Env_main_0 *env) {
     return 1 + env->x; // simplified for readability
-}
-
-// Deleter for this particular closure
-void delete_Closure__Unit_Int__Int(Closure__Unit_Int closure) {
-    free(closure.env);
 }
 
 int main() {
@@ -33,13 +33,14 @@ int main() {
     // let f
     Env_main_0 *env_0 = malloc(sizeof(Env_main_0));
     env_0->x = x;
-    Closure__Unit_Int f = {
+    Closure f = {
         .callback = (void*)lifted_lambda_main_0,
-        .env = env_0
+        .env = env_0,
+        .delete = (void*)delete_Env_main_0
     };
     // call f
     int _1 = f.callback(f.env);
     // delete f
-    delete_Closure__Unit_Int__Int(f);
+    f.delete(f.env);
     return _1;
 }
