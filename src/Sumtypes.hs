4745217bd7cdcf2009466b91940f36866b018caa
Erik Svedäng <erik.svedang@gmail.com>
2018-10-30 12:55:17 +0100

    Sumtype module.

---
+++ src/Sumtypes.hs
@@ -0,0 +1,16 @@
+module Sumtypes where
+
+import qualified Data.Map as Map
+import Data.Maybe
+import Debug.Trace
+
+import Obj
+import Types
+import Util
+--import Template
+--import ToTemplate
+--import Infer
+--import Concretize
+--import Polymorphism
+--import ArrayTemplates
+--import Lookup
