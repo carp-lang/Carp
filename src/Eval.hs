3c033204d816001c0e6541218b62967fafea92e2
hellerve <veit@veitheller.de>
2020-04-18 12:15:05 +0200

    eval: guard against calling main directly

--- src/Eval.hs
+++ src/Eval.hs
@@ -409,6 +409,9 @@ executeCommand ctx@(Context env _ _ _ _ _ _ _) xobj =
          reportExecutionError newCtx (show e)
          return (xobj, newCtx)
        -- special case: calling a function at the repl
+       Right (XObj (Lst (XObj (Lst (XObj (Defn _) _ _:(XObj (Sym (SymPath [] "main") _) _ _):_)) _ _:_)) _ _) -> do
+        reportExecutionError newCtx "`main` should not be called from the REPL. Use `(build)` and `(run)` instead."
+        return (xobj, newCtx)
        Right (XObj (Lst (XObj (Lst (XObj (Defn _) _ _:name:_)) _ _:args)) i _) -> do
         (nc, r) <- annotateWithinContext False newCtx (XObj (Lst (name:args)) i Nothing)
         case r of
