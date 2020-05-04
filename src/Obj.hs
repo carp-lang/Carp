5168c3b9b25976a987d1dda95b3a3ec8539e9c34
scottolsen <scg.olsen@gmail.com>
2020-05-04 11:33:27 -0400

    Update Obj<->Ty handling for Structs
    Now that structs may have a variable in the constructor position, I've
    updated the Obj to Ty (and inverse) translations to account for vars or
    concrete type names.

--- src/Obj.hs
+++ src/Obj.hs
@@ -505,8 +505,8 @@ replaceGenericTypeSymbols _ xobj = xobj
 -- | Convert a Ty to the s-expression that represents that type.
 -- | TODO: Add more cases and write tests for this.
 tyToXObj :: Ty -> XObj
-tyToXObj (StructTy n []) = XObj (Sym (SymPath [] n) Symbol) Nothing Nothing
-tyToXObj (StructTy n vs) = XObj (Lst (XObj (Sym (SymPath [] n) Symbol) Nothing Nothing : map tyToXObj vs)) Nothing Nothing
+tyToXObj (StructTy n []) = tyToXObj n
+tyToXObj (StructTy n vs) = XObj (Lst (tyToXObj n : map tyToXObj vs)) Nothing Nothing
 tyToXObj (RefTy t lt) = XObj (Lst [XObj (Sym (SymPath [] "Ref") Symbol) Nothing Nothing, tyToXObj t, tyToXObj lt]) Nothing Nothing
 tyToXObj (PointerTy t) = XObj (Lst [XObj (Sym (SymPath [] "Ptr") Symbol) Nothing Nothing, tyToXObj t]) Nothing Nothing
 tyToXObj (FuncTy argTys returnTy StaticLifetimeTy) = XObj (Lst [XObj (Sym (SymPath [] "Fn") Symbol) Nothing Nothing, XObj (Arr (map tyToXObj argTys)) Nothing Nothing, tyToXObj returnTy]) Nothing Nothing
@@ -711,7 +711,7 @@ xobjToTy (XObj (Sym (SymPath _ "Char") _) _ _) = Just CharTy
 xobjToTy (XObj (Sym (SymPath _ "Bool") _) _ _) = Just BoolTy
 xobjToTy (XObj (Sym (SymPath _ "Static") _) _ _) = Just StaticLifetimeTy
 xobjToTy (XObj (Sym (SymPath _ s@(firstLetter:_)) _) _ _) | isLower firstLetter = Just (VarTy s)
-                                                          | otherwise = Just (StructTy s [])
+                                                          | otherwise = Just (StructTy (ConcreteNameTy s) [])
 xobjToTy (XObj (Lst [XObj (Sym (SymPath _ "Ptr") _) _ _, innerTy]) _ _) =
   do okInnerTy <- xobjToTy innerTy
      return (PointerTy okInnerTy)
@@ -747,8 +747,8 @@ xobjToTy (XObj (Lst (x:xs)) _ _) =
   do okX <- xobjToTy x
      okXS <- mapM xobjToTy xs
      case okX of
-       (StructTy n []) -> return (StructTy n okXS)
-       (VarTy n) -> return (StructTy n okXS) -- Struct type with type variable as a name, i.e. "(a b)"
+       (StructTy c@(ConcreteNameTy n) []) -> return (StructTy c okXS)
+       v@(VarTy n) -> return (StructTy v okXS) -- Struct type with type variable as a name, i.e. "(a b)"
        _ -> Nothing
 xobjToTy _ = Nothing
 
@@ -838,10 +838,10 @@ defineFunctionTypeAlias :: Ty -> XObj
 defineFunctionTypeAlias aliasTy = defineTypeAlias (tyToC aliasTy) aliasTy
 
 defineArrayTypeAlias :: Ty -> XObj
-defineArrayTypeAlias t = defineTypeAlias (tyToC t) (StructTy "Array" [])
+defineArrayTypeAlias t = defineTypeAlias (tyToC t) (StructTy (ConcreteNameTy "Array") [])
 
 defineStaticArrayTypeAlias :: Ty -> XObj
-defineStaticArrayTypeAlias t = defineTypeAlias (tyToC t) (StructTy "Array" [])
+defineStaticArrayTypeAlias t = defineTypeAlias (tyToC t) (StructTy (ConcreteNameTy "Array") [])
 
 -- |
 defineInterface :: String -> Ty -> [SymPath] -> Maybe Info -> XObj
