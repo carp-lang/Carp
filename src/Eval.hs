9520e90d7858a6843f9907aa6657a18da98d4588
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-05-08 16:35:11 +0200

    Added cases to `primitiveDefmodule` to gracefully handle invalid usages.

--- src/Eval.hs
+++ src/Eval.hs
@@ -553,6 +553,10 @@ primitiveDefmodule xobj ctx@(Context env i typeEnv pathStrings proj lastInput ex
                  case result of
                    Left err -> return (newCtx, Left err)
                    Right _ -> return (newCtx, r)
+primitiveDefmodule _ ctx (x:_) =
+  return (evalError ctx ("`defmodule` expects a Symbol, got '" ++ pretty x ++ "' instead.") (info x))
+primitiveDefmodule _ ctx [] =
+  return (evalError ctx "`defmodule` requires a Symbol and some expressions, received none." (Just dummyInfo))
 
 -- | "NORMAL" COMMANDS (just like the ones in Command.hs, but these need access to 'eval', etc.)
 
