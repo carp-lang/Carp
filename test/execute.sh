eec1ad93751d9d9b72afacea132c01353b372895
hellerve <veit@veitheller.de>
2020-05-12 16:01:42 +0200

    test: rollback accidently commited test update

--- test/execute.sh
+++ test/execute.sh
@@ -2,7 +2,7 @@
 
 # Runs the executable and compares its output to the .expected file
 ./carp.sh $1 --log-memory -b && \
-  ~/.carp/out/Untitled > test/output/$1.output.actual 2>&1
+  ./out/Untitled > test/output/$1.output.actual 2>&1
 echo $1
 
 if ! diff --strip-trailing-cr test/output/$1.output.actual test/output/$1.output.expected; then
