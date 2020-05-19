000befe264e0f3512a71078384cf3fd6fb370e81
scottolsen <scg.olsen@gmail.com>
2020-05-19 16:03:37 -0400

    Update primitiveMeta; make primitiveImplements take a list
    This commit does two things:
    
    1. Updates `primitiveMeta` to handle a previously unhandled case, a
       qualified name in a module.
    
       Before `meta` use to fail to find its argument binding in a cases
       like this one:
    
       ```
       (defmodule Foo (hidden bar) (meta Foo.bar "hidden"))
       ```
    
       This is because `meta` used to cons path strings to the path even if
       the path was already qualified with a module name as in the example
       above. We now treat these cases separately, and instances like the
       above work.
    
    2. Update `primitiveImplements` to set the implements meta to a dynamic
       list of interface names, instead of a single interface.
    
       This makes sense since functions could implement multiple generic
       interfaces. An example of the new behavior:
    
       ```
       (implements inc foo)
       (implements dec foo)
       (meta foo "implements")
       => (dec inc)
       ```

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -138,9 +138,19 @@ primitiveImplements xobj ctx [x@(XObj (Sym interface@(SymPath _ _) _) _ _), i@(X
                   _ -> putStrLnWithColor Red err
                 return $ evalError ctx err (info x)
            Right ctx' ->
-             let adjustedMeta = meta {getMeta = Map.insert "implements" x (getMeta meta)}
-             in  return (ctx' {contextGlobalEnv = envInsertAt global (getPath defobj) (Binder adjustedMeta defobj)},
+             do currentImplementations <- primitiveMeta xobj ctx [i, XObj (Str "implements") (Just dummyInfo) (Just StringTy)]
+                case snd currentImplementations of
+                  Left err  -> return $ (ctx, Left err)
+                  Right (XObj (Lst impls) inf ty) ->
+                    let newImpls = XObj (Lst (x : impls)) inf (Just DynamicTy)
+                        adjustedMeta = meta {getMeta = Map.insert "implements" newImpls (getMeta meta)}
+                    in  return (ctx' {contextGlobalEnv = envInsertAt global (getPath defobj) (Binder adjustedMeta defobj)},
                                dynamicNil)
+                  _ ->
+                    let impls = XObj (Lst [x]) (Just dummyInfo) (Just UnitTy)
+                        adjustedMeta = meta {getMeta = Map.insert "implements" impls (getMeta meta)}
+                    in  return (ctx' {contextGlobalEnv = envInsertAt global (getPath defobj) (Binder adjustedMeta defobj)},
+                                 dynamicNil)
      -- If the implementation binding doesn't exist yet, set the implements
      -- meta. This enables specifying a function as an implementation before
      -- defining it.
@@ -153,7 +163,7 @@ primitiveImplements xobj ctx [x@(XObj (Sym interface@(SymPath _ _) _) _ _), i@(X
                                       " at " ++ prettyInfoFromXObj xobj ++ " is not defined." ++
                                       " Did you define it using `definterface`?")
                 putStrLnWithColor White "" -- To restore color for sure.
-         primitiveMetaSet xobj ctx [i, XObj (Str "implements") (Just dummyInfo) (Just StringTy), x]
+         primitiveMetaSet xobj ctx [i, XObj (Str "implements") (Just dummyInfo) (Just StringTy), XObj (Lst [x]) (Just dummyInfo) (Just DynamicTy)]
 primitiveImplements xobj ctx [x, y] =
   return $ evalError ctx ("`implements` expects symbol arguments.") (info x)
 primitiveImplements x@(XObj _ i t) ctx args =
@@ -617,11 +627,24 @@ primitiveUse xobj ctx [XObj (Sym path _) _ _] = do
 
 -- | Get meta data for a Binder
 primitiveMeta :: Primitive
-primitiveMeta (XObj _ i _) ctx [XObj (Sym path _) _ _, XObj (Str key) _ _] = do
+primitiveMeta (XObj _ i _) ctx [XObj (Sym path@(SymPath [] p) _) _ _, XObj (Str key) _ _] = do
   let pathStrings = contextPath ctx
       fppl = projectFilePathPrintLength (contextProj ctx)
       globalEnv = contextGlobalEnv ctx
   case lookupInEnv (consPath pathStrings path) globalEnv of
+    Just (_, Binder metaData _) ->
+        case Map.lookup key (getMeta metaData) of
+          Just foundValue ->
+            return (ctx, Right foundValue)
+          Nothing ->
+            return (ctx, dynamicNil)
+    Nothing ->
+      return (evalError ctx
+                        ("`meta` failed, I can’t find `" ++ show path ++ "`")
+                        i)
+primitiveMeta (XObj _ i _) ctx [XObj (Sym path _) _ _, XObj (Str key) _ _] = do
+  let globalEnv = contextGlobalEnv ctx
+  case lookupInEnv path globalEnv of
     Just (_, Binder metaData _) ->
         case Map.lookup key (getMeta metaData) of
           Just foundValue ->
