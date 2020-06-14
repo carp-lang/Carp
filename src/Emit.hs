fd89b64425d54b8cf11f25444beb3111a6aa8968
Lucas Leblow <leblowl@gmail.com>
2020-06-13 18:05:37 -0600

    Attempt 2; only delete values, not refs

--- src/Emit.hs
+++ src/Emit.hs
@@ -371,6 +371,8 @@ toC toCMode (Binder meta root) = emitterSrc (execState (visit startingIndent roo
                        when isNotVoid $
                          appendToSrc (addIndent indent' ++ retVar ++ " = " ++ caseExprRetVal ++ ";\n")
                        let Just caseLhsInfo' = caseLhsInfo
+                       when (matchMode == MatchValue)
+                         (delete indent' caseLhsInfo')
                        appendToSrc (addIndent indent ++ "}\n")
 
               in  do exprVar <- visit indent expr
