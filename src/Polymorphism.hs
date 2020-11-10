47dfd5900b4ea72c34c2b4344560a1b403e2a4f9
Erik Svedang <erik@Eriks-iMac.local>
2020-11-10 23:14:26 +0100

    fix: Make nameOfPolymorphicFunction not add so suffix to external functions

--- src/Polymorphism.hs
+++ src/Polymorphism.hs
@@ -21,6 +21,8 @@ nameOfPolymorphicFunction typeEnv env functionType functionName =
   let foundBinders = multiLookupALL functionName env
   in case filter ((\(Just t') -> areUnifiable functionType t') . ty . binderXObj . snd) foundBinders of
        [] -> Nothing
+       [(_, Binder _ (XObj (Lst (XObj (External (Just name)) _ _ : _)) _ _))] ->
+         Just (SymPath [] name)
        [(_, Binder _ single)] ->
          let Just t' = ty single
              (SymPath pathStrings name) = getPath single
