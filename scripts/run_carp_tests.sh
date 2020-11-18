428dce944529c158f10172ddb306f7f24981314b
Erik Svedäng <erik.svedang@gmail.com>
2020-11-18 22:29:13 +0100

    fix: Use 'sh' syntax in run_carp_tests.sh

--- scripts/run_carp_tests.sh
+++ scripts/run_carp_tests.sh
@@ -4,7 +4,7 @@ set -e; # will make the script stop if there are any errors
 set -u; # will make the script stop if there is use of undefined
 
 NO_SDL=0
-if [[ $# -gt 0 ]] && [[ "$1" == "--no_sdl" ]]; then
+if [ $# -gt 0 ] && [ "$1" = "--no_sdl" ]; then
     NO_SDL=1
 fi
 
@@ -54,16 +54,16 @@ echo ./examples/no_core.carp
 ./scripts/carp.sh ./examples/no_core.carp --no-core --no-profile -b
 
 # Run tests which rely on SDL unless the `--no_sdl` argument was passed in
-if [[ ${NO_SDL} -eq 0 ]]; then
+if [ ${NO_SDL} -eq 0 ]; then
     echo "Compile-only SDL examples"
     compileOnlySdlExamples="
-			    examples/ant.carp
+                examples/ant.carp
                             examples/reptile.carp
                             examples/game.carp
                             examples/minimal_sdl.carp
                             examples/sounds.carp
                             examples/fonts.carp
-			    "
+                "
 
     for e in $compileOnlySdlExamples ; do
         echo $e
@@ -75,7 +75,7 @@ fi
 ./scripts/carp.sh ./docs/core/generate_core_docs.carp
 
 # Generate SDL docs unless the `--no_sdl` argument was passed in
-if [[ ${NO_SDL} -eq 0 ]]; then
+if [ ${NO_SDL} -eq 0 ]; then
   ./scripts/carp.sh ./docs/sdl/generate_sdl_docs.carp
 fi
 
