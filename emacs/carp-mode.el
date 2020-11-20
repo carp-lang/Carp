ffa30801ac4058ef80ef2acf2f20a2e8d023367a
Erik Svedäng <erik.svedang@gmail.com>
2020-11-20 07:44:45 +0100

    refactor: Move emacs-mode to separate repo

--- emacs/carp-mode.el
+++
@@ -1,7 +0,0 @@
-
-(define-derived-mode carp-mode clojure-mode "Carp"
-  "Major mode for the Carp programming language.")
-
-(add-to-list 'auto-mode-alist '("\\.carp\\'" . carp-mode))
-
-(provide 'carp-mode)
