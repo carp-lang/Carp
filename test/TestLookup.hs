5c269c4e66929d22d14f8bcb8d69d81b1da7ef57
Erik Svedäng <erik.svedang@gmail.com>
2020-12-05 23:05:16 +0100

    test: First, very simple test

--- test/TestLookup.hs
+++ test/TestLookup.hs
@@ -1,8 +1,23 @@
 module TestLookup where
 
+import qualified Data.Map as Map
+import qualified Lookup as Lookup
+import Obj
 import Test.HUnit
+import Types
 
-testLookup = [something]
+testLookup :: [Test]
+testLookup =
+  [ basicLookup
+  ]
 
-something =
-  TestCase $ assertBool "Failure" False
+b1 = Binder emptyMeta (XObj (Str "b1") Nothing (Just StringTy))
+
+emptyRootEnv = Env (Map.fromList []) Nothing Nothing [] ExternalEnv 0
+
+assertNotFound :: Maybe Binder -> Test
+assertNotFound Nothing = TestCase (assertBool "assertNotFound" True) -- Better way?
+assertNotFound _ = TestCase (assertBool "assertNotFound" False)
+
+basicLookup :: Test
+basicLookup = assertNotFound (fmap snd (Lookup.lookupInEnv (SymPath [] "nonexisting") emptyRootEnv))
