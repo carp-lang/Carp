6a49318c995881aeb03361a805891033c4b77671
Erik Svedäng <erik.svedang@gmail.com>
2020-12-10 07:05:51 +0100

    fix: Manage String and Pattern. Tests run!

--- src/Managed.hs
+++ src/Managed.hs
@@ -18,10 +18,18 @@ isExternalType typeEnv (StructTy (ConcreteNameTy name) _) =
 isExternalType _ _ =
   False
 
--- | Is this type managed - does it need to be freed?
+-- | Should this type be handled by the memory management system.
+-- Implementation note: This top-level pattern match should be able to just
+-- match on all types and see whether they implement 'delete', but for some
+-- reson that doesn't work. Might need to handle generic types separately?
 isManaged :: TypeEnv -> Env -> Ty -> Bool
 isManaged typeEnv globalEnv structTy@StructTy{} =
   interfaceImplementedForTy typeEnv globalEnv "delete" (FuncTy [structTy] UnitTy StaticLifetimeTy)
 isManaged typeEnv globalEnv funcTy@FuncTy{} =
   interfaceImplementedForTy typeEnv globalEnv "delete" (FuncTy [funcTy] UnitTy StaticLifetimeTy)
-isManaged _ _ _ = False
+isManaged _ _ StringTy =
+  True
+isManaged _ _ PatternTy =
+  True
+isManaged _ _ _ =
+  False
