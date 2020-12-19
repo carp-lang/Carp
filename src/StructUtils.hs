653e26dd5c6282d1107c72e76f0f181454d1127d
Erik Svedäng <erik.svedang@gmail.com>
2020-12-19 13:54:41 +0100

    refactor: Decide if struct member takes ref or not when printing
    ..based on blitable interface, and 'prn' implemntation

--- src/StructUtils.hs
+++ src/StructUtils.hs
@@ -1,29 +1,33 @@
 module StructUtils where
 
-import Managed
+import Interfaces
 import Obj
 import Polymorphism
 import Types
 
 -- | The 'str'/'prn' functions for primitive types don't take refs, while other types do
--- so we need to adjust for that when finding and calling them in compound types
-memberInfo :: TypeEnv -> Env -> Ty -> (Ty, String, Ty)
-memberInfo typeEnv globalEnv memberTy =
-  let refOrNotRefType =
-        if isManaged typeEnv globalEnv memberTy
-          then RefTy memberTy (VarTy "w") -- OBS! The VarTy "w" here is dubious
-          else memberTy
-   in ( refOrNotRefType,
-        if isManaged typeEnv globalEnv memberTy
-          then "&"
-          else "",
-        FuncTy [refOrNotRefType] StringTy StaticLifetimeTy
-      )
+-- so we need to adjust for that when finding and calling them in compound types.
+-- The returned tuple contains ("" || "&", `str function type`).
+memberStrCallingConvention :: String -> TypeEnv -> Env -> Ty -> (String, Ty)
+memberStrCallingConvention strOrPrn typeEnv globalEnv memberTy =
+  if callWithValue
+    then ("", withValueSig)
+    else ("&", withRefSig)
+  where
+    withValueSig = FuncTy [memberTy] StringTy StaticLifetimeTy
+    withRefSig = FuncTy [RefTy memberTy (VarTy "w")] StringTy StaticLifetimeTy -- "w" here is dubious?
+    callWithValue =
+      -- If these interfaces are not implemented we assume ref signature is fine.
+      -- Blitable is required to not accicentally pass a value owned by the struct to
+      -- a non-ref str/prn function implemented on one of its memeber types.
+      let blitable = interfaceImplementedForTy typeEnv globalEnv "blitable" (FuncTy [memberTy] memberTy StaticLifetimeTy)
+          strTakesValue = interfaceImplementedForTy typeEnv globalEnv strOrPrn withValueSig
+       in blitable && strTakesValue
 
 -- | Generate C code for converting a member variable to a string and appending it to a buffer.
 memberPrn :: TypeEnv -> Env -> (String, Ty) -> String
 memberPrn typeEnv env (memberName, memberTy) =
-  let (_, maybeTakeAddress, strFuncType) = memberInfo typeEnv env memberTy
+  let (prefix, strFuncType) = memberStrCallingConvention "prn" typeEnv env memberTy
    in case nameOfPolymorphicFunction typeEnv env strFuncType "prn" of
         Just strFunctionPath ->
           case strFuncType of
@@ -36,7 +40,7 @@ memberPrn typeEnv env (memberName, memberTy) =
                 ]
             _ ->
               unlines
-                [ "  temp = " ++ pathToC strFunctionPath ++ "(" ++ maybeTakeAddress ++ "p->" ++ memberName ++ ");",
+                [ "  temp = " ++ pathToC strFunctionPath ++ "(" ++ prefix ++ "p->" ++ memberName ++ ");",
                   "  sprintf(bufferPtr, \"%s \", temp);",
                   "  bufferPtr += strlen(temp) + 1;",
                   "  if(temp) { CARP_FREE(temp); temp = NULL; }"
@@ -47,7 +51,7 @@ memberPrn typeEnv env (memberName, memberTy) =
 -- | Calculate the size for prn:ing a member of a struct
 memberPrnSize :: TypeEnv -> Env -> (String, Ty) -> String
 memberPrnSize typeEnv env (memberName, memberTy) =
-  let (_, maybeTakeAddress, strFuncType) = memberInfo typeEnv env memberTy
+  let (prefix, strFuncType) = memberStrCallingConvention "prn" typeEnv env memberTy
    in case nameOfPolymorphicFunction typeEnv env strFuncType "prn" of
         Just strFunctionPath ->
           case strFuncType of
@@ -59,7 +63,7 @@ memberPrnSize typeEnv env (memberName, memberTy) =
                 ]
             _ ->
               unlines
-                [ "  temp = " ++ pathToC strFunctionPath ++ "(" ++ maybeTakeAddress ++ "p->" ++ memberName ++ "); ",
+                [ "  temp = " ++ pathToC strFunctionPath ++ "(" ++ prefix ++ "p->" ++ memberName ++ "); ",
                   "  size += snprintf(NULL, 0, \"%s \", temp);",
                   "  if(temp) { CARP_FREE(temp); temp = NULL; }"
                 ]
